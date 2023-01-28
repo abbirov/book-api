93c36c7260936a9abd77a9c23beb6ae3e4ee2519
Abdullokh <abdullooffical@gmail.com>
2023-01-29 01:38:03 +0500

    added (user controllers)

---
+++ src/Controller/UserFullNameAction.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Component\User\FullNameDto;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class UserFullNameAction extends AbstractController
+{
+    public function __invoke(Request $request, SerializerInterface $serializer): FullNameDto
+    {
+        return $serializer->deserialize($request->getContent(), FullNameDto::class, 'json');
+    }
+}
