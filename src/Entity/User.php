85e734fbf23b6c61c92114a0c1d9b88b32c2646d
Abdullokh <abdullooffical@gmail.com>
2023-01-29 02:12:00 +0500

    added (jwt auth), updated security.yaml

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -47,7 +47,7 @@ class User implements PasswordAuthenticatedUserInterface, UserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['user:read', 'user:wite'])]
+    #[Groups(['user:read', 'user:write'])]
     private ?string $email = null;
 
     #[ORM\Column(length: 255)]
