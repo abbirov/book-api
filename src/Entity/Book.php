1e5253e3b91e7be5472263e9c1631ed3e15b309d
Abdullokh <abdullooffical@gmail.com>
2023-01-31 08:40:25 +0500

    added (api filters) to book

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -2,6 +2,9 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\BookRepository;
 use Doctrine\DBAL\Types\Types;
@@ -9,6 +12,8 @@ use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
 #[ApiResource]
+#[ApiFilter(SearchFilter::class, properties: ['id' => 'exact', 'category' => 'exact'])]
+#[ApiFilter(OrderFilter::class, properties: ['id', 'name'])]
 class Book
 {
     #[ORM\Id]
